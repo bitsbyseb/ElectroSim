f8787d50d5dae5df7453060fcea4a6ced7e4f673
Sebastián Puentes <68166050+bitsbyseb@users.noreply.github.com>
2025-11-07 17:12:54 -0500

    Fix backend URL formatting
    Removed trailing slash from backend URL.

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const backendURL = import.meta.env.DEV ? "http://localhost:3000" : "https://web-po2h1tj3uehn.up-de-fra1-k8s-1.apps.run-on-seenode.com/" ;
+export const backendURL = import.meta.env.DEV ? "http://localhost:3000" : "https://web-po2h1tj3uehn.up-de-fra1-k8s-1.apps.run-on-seenode.com" ;
